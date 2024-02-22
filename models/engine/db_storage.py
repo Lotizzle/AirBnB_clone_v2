adb1037460f793facbddbe2479ab986abcfc944a
Lotizzle <lotharmbaegbu@gmail.com>
2024-02-22 13:01:24 +0000

    Fixed db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -69,7 +69,7 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         """Closes the session"""
